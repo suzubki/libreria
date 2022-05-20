f69d8f809aa569a11abfc6a4fbc161632c63e700
Darwin_N19 <82281869+suzubki@users.noreply.github.com>
2022-05-20 15:29:58 -0500

    prob

--- db/knexfile.js
+++ db/knexfile.js
@@ -7,7 +7,7 @@ const database = knex({
     client: "pg",
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized },
+        ssl: { rejectUnauthorized: false },
     },
 });
 // connection: process.env.DATABASE_URL || {
