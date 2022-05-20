768aecd052a749fbe5da170ad250dc1f9dacd44c
Darwin_N19 <82281869+suzubki@users.noreply.github.com>
2022-05-19 21:56:11 -0500

    probando

--- db/knexfile.js
+++ db/knexfile.js
@@ -8,6 +8,7 @@ const database = knex({
     connection: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
+        port: process.env.DB_PORT,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
     },
