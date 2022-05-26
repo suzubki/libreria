a36f01b158295e40e0432e12ab50c3d3fe9fc674
Darwin_N19 <82281869+suzubki@users.noreply.github.com>
2022-05-25 22:01:11 -0500

    cors add

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import routes from "./routes/index.js";
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/", routes);
 
 app.listen(PORT, () => console.log("Listening on Port + " + PORT));
